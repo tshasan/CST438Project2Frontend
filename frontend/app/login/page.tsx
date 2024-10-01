169673e2bf0169883d2b91fb3611849db3fb7194
codycodefalco <codyfalconer023@gmail.com>
2024-10-01 12:57:59 -0700

    signupPage added
    Added frontend for signup page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,9 +1,13 @@
-import DebugRoute from "@/components/debugroute";
+import DebugRoute from "@/components/common/debugroute";
+import LoginPage from "@/components/pages/loginpage";
+import loginpage from "@/components/pages/loginpage"
 
 export default function Home() {
   return (
     <div>
+      <header/>
       <h1>login Page</h1> {/* Placeholder to make sure routing is working */}
+      <LoginPage/>
       <DebugRoute />
     </div>
   );
